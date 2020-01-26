202f9b1f96c49144079e6cee6483fae4d7dfac35
Ali Hassan <ali.software.dev@gmail.com>
2020-01-26 18:55:37 +0000

    Just started, added search tab

---
+++ DiscoverPlaces/DiscoverPlaces/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  DiscoverPlaces
+//
+//  Created by user on 26/01/2020.
+//  Copyright © 2020 AHApps. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Search")
+        ]
+        
+    }
+
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageIconName: String? = nil) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .gray
+        
+        return navController
+    }
+}
