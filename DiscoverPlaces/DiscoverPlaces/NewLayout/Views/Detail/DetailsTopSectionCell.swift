e65883120a77b1bba3ed1314d15122dc44b25eae
Ali Hassan <ali.software.dev@gmail.com>
2020-02-09 19:00:15 +0000

    Added more places section, Arranged folders

---
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/DetailsTopSectionCell.swift
@@ -0,0 +1,67 @@
+//
+//  TopSectionDetailsCell.swift
+//  DiscoverPlaces
+//
+//  Created by user on 09/02/2020.
+//  Copyright © 2020 AHApps. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTopSectionCell: UICollectionViewCell {
+    
+    var place: Result? {
+        didSet {
+            placeNameLabel.text = place?.name
+            addressLabel.text = place?.vicinity
+        }
+    }
+    
+    let placeNameLabel = UILabel(text: "Burj Al Arab", font: .boldSystemFont(ofSize: 24), color: .label, alignment: .center, numberOfLines: 3)
+    
+    let addressLabel = UILabel(text: "123 Buckingham Place Place, Victoria, London, United Kingdom, E17 7AJ", font: .systemFont(ofSize: 19), color: .secondaryLabel, alignment: .center, numberOfLines: 0)
+    
+    let starView: UIView! = {
+        let v = UIView()
+        v.backgroundColor = Constants.primaryHighlightColor
+        v.constrainHeight(constant: 22)
+        v.constrainWidth(constant: 115)
+        return v
+    }()
+    
+    let reviewsButton: UIButton! = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "reviewsButton"), for: .normal)
+        btn.imageView?.contentMode = .scaleAspectFit
+        btn.constrainWidth(constant: 80)
+        return btn
+    }()
+    
+//    let padding1 = UIView()
+//    let padding2 = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = Constants.primaryHighlightColor
+        
+        let ratingsStackView = UIStackView(arrangedSubviews: [UIView(), starView, reviewsButton, UIView()])
+        ratingsStackView.spacing = 6
+        
+        let topStackView = VerticalStackView(arrangedSubviews: [placeNameLabel,
+                                                                       addressLabel,
+                                                                       ratingsStackView], spacing: 8)
+        topStackView.alignment = .center
+        
+        
+        addSubview(topStackView)
+        topStackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 16, left: 24, bottom: 0, right: 24))
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/MorePlacesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MorePlacesCell.swift
+//  DiscoverPlaces
+//
+//  Created by user on 09/02/2020.
+//  Copyright © 2020 AHApps. All rights reserved.
+//
+
+import UIKit
+
+class MorePlacesCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .gray
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
