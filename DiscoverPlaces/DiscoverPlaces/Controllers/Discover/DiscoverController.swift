7a454221abfa1f2e98b67d59bda9925605e024b8
Ali Hassan <ali.software.dev@gmail.com>
2020-02-15 19:39:01 +0000

    Commented out old discover code

--- DiscoverPlaces/DiscoverPlaces/Controllers/Discover/DiscoverController.swift
+++ DiscoverPlaces/DiscoverPlaces/Controllers/Discover/DiscoverController.swift
@@ -1,101 +1,101 @@
+////
+////  DiscoverController.swift
+////  DiscoverPlaces
+////
+////  Created by user on 28/01/2020.
+////  Copyright © 2020 AHApps. All rights reserved.
+////
 //
-//  DiscoverController.swift
-//  DiscoverPlaces
+//import UIKit
 //
-//  Created by user on 28/01/2020.
-//  Copyright © 2020 AHApps. All rights reserved.
+//class DiscoverController: BaseCollectionViewController, UICollectionViewDelegateFlowLayout {
 //
-
-import UIKit
-
-class DiscoverController: BaseCollectionViewController, UICollectionViewDelegateFlowLayout {
-
-    fileprivate let selectedCellId = "selectedId"
-    fileprivate let nearbyCellId = "headerId"
-    fileprivate let cellId = "cellId"
-    
-    var categories: [Categoryy]! {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        categories = getCategories()
-        
-        collectionView.backgroundColor = .white
-        //Header step 1
-        collectionView.register(NearbyHolder.self, forCellWithReuseIdentifier: nearbyCellId)
-        collectionView.register(DiscoverCardsHolder.self, forCellWithReuseIdentifier: cellId)
-        collectionView.register(SelectedCategoriesHolder.self, forCellWithReuseIdentifier: selectedCellId)
-    }
-    
-    func getCategories() -> [Categoryy] {
-        
-        //Get fron user defaults
-        let cats = [Categoryy(name: "amusement_park", isSelected: true),
-                    Categoryy(name: "museum", isSelected: true),
-                    Categoryy(name: "mosque", isSelected: true),
-                    Categoryy(name: "clothing_store", isSelected: true),
-                    Categoryy(name: "tourist_attraction", isSelected: true)
-        ]
-        
-        return cats
-    }
-}
-
-extension DiscoverController {
-  
-    //Delegate & DataSource
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count + 2 //(The 2 is the categories selector + nearby controller)
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        switch indexPath.item {
-        case 0:
-            //Categories Selector
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: selectedCellId, for: indexPath) as! SelectedCategoriesHolder
-            cell.horizontalController.categories = categories
-            cell.horizontalController.didSelectHandler = { [weak self] category, index in
-                print("Tapped on: \(String(describing: category.name))")
-                cell.horizontalController.toggleSelectedState(index: index)
-            }
-            return cell
-        case 1:
-            //Nearby header
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: nearbyCellId, for: indexPath) as! NearbyHolder
-                return cell
-        default:
-            //Category results
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! DiscoverCardsHolder
-            cell.sectionTitle.text = categories[indexPath.item - 2].name
-            cell.horizontalController.category = categories[indexPath.item - 2].name
-            return cell
-        }
-    }
-    
-    //Layout
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        switch indexPath.item {
-        case 0:
-            //Categories Selector
-            return .init(width: view.frame.width, height: 60)
-        case 1:
-            //Nearby header
-            return .init(width: view.frame.width, height: 220)
-        default:
-            //Category results
-            return .init(width: view.frame.width, height: 160)
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 5
-    }
-    
-}
+//    fileprivate let selectedCellId = "selectedId"
+//    fileprivate let nearbyCellId = "headerId"
+//    fileprivate let cellId = "cellId"
+//    
+//    var categories: [Categoryy]! {
+//        didSet {
+//            collectionView.reloadData()
+//        }
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        categories = getCategories()
+//        
+//        collectionView.backgroundColor = .white
+//        //Header step 1
+//        collectionView.register(NearbyHolder.self, forCellWithReuseIdentifier: nearbyCellId)
+//        collectionView.register(DiscoverCardsHolder.self, forCellWithReuseIdentifier: cellId)
+//        collectionView.register(SelectedCategoriesHolder.self, forCellWithReuseIdentifier: selectedCellId)
+//    }
+//    
+//    func getCategories() -> [Categoryy] {
+//        
+//        //Get fron user defaults
+//        let cats = [Categoryy(name: "amusement_park", isSelected: true),
+//                    Categoryy(name: "museum", isSelected: true),
+//                    Categoryy(name: "mosque", isSelected: true),
+//                    Categoryy(name: "clothing_store", isSelected: true),
+//                    Categoryy(name: "tourist_attraction", isSelected: true)
+//        ]
+//        
+//        return cats
+//    }
+//}
+//
+//extension DiscoverController {
+//  
+//    //Delegate & DataSource
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return categories.count + 2 //(The 2 is the categories selector + nearby controller)
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//        switch indexPath.item {
+//        case 0:
+//            //Categories Selector
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: selectedCellId, for: indexPath) as! SelectedCategoriesHolder
+//            cell.horizontalController.categories = categories
+//            cell.horizontalController.didSelectHandler = { [weak self] category, index in
+//                print("Tapped on: \(String(describing: category.name))")
+//                cell.horizontalController.toggleSelectedState(index: index)
+//            }
+//            return cell
+//        case 1:
+//            //Nearby header
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: nearbyCellId, for: indexPath) as! NearbyHolder
+//                return cell
+//        default:
+//            //Category results
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! DiscoverCardsHolder
+//            cell.sectionTitle.text = categories[indexPath.item - 2].name
+//            cell.horizontalController.category = categories[indexPath.item - 2].name
+//            return cell
+//        }
+//    }
+//    
+//    //Layout
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        
+//        switch indexPath.item {
+//        case 0:
+//            //Categories Selector
+//            return .init(width: view.frame.width, height: 60)
+//        case 1:
+//            //Nearby header
+//            return .init(width: view.frame.width, height: 220)
+//        default:
+//            //Category results
+//            return .init(width: view.frame.width, height: 160)
+//        }
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 5
+//    }
+//    
+//}
--- DiscoverPlaces/DiscoverPlaces/Controllers/Discover/DiscoverGroupHorizontalController.swift
+++ DiscoverPlaces/DiscoverPlaces/Controllers/Discover/DiscoverGroupHorizontalController.swift
@@ -1,81 +1,81 @@
+////
+////  DiscoverGroupController.swift
+////  DiscoverPlaces
+////
+////  Created by user on 28/01/2020.
+////  Copyright © 2020 AHApps. All rights reserved.
+////
 //
-//  DiscoverGroupController.swift
-//  DiscoverPlaces
+//import UIKit
 //
-//  Created by user on 28/01/2020.
-//  Copyright © 2020 AHApps. All rights reserved.
+//class DiscoverGroupHorizontalController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout {
+//    
+//    var category: String! {
+//        didSet {
+//            fetchPlaces(for: category)
+//        }
+//    }
+//    
+//    var placeResults = [PlaceResult]()
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        collectionView.backgroundColor = .white
+//        collectionView.register(DicoverCardCell.self, forCellWithReuseIdentifier: "id")
+//        collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+//        
+//        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+//            layout.scrollDirection = .horizontal
+//        }
+//        
+//    }
+//    
+//    fileprivate func fetchPlaces(for category: String) {
+//        Service.shared.fetchPlacesForCategory(for: category) { (results, error) in
+//            
+//            if let error = error {
+//                print("Failed to Fetch: \(error.localizedDescription)")
+//                return
+//            }
+//            
+//            //success
+//            var filteredResults = [PlaceResult]()
+//            
+//            results?.results.forEach({ (result) in
+//                if result.containsPhotos() {
+//                    filteredResults.append(result)
+//                }
+//            })
+//            
+//            self.placeResults = filteredResults
+//            
+//            
+//            DispatchQueue.main.async {
+//                if self.placeResults.count == 0 {
+//                    //PlaceHolder or alternative?
+//                    return
+//                } else {
+//                    self.collectionView.reloadData()
+//                }
+//            }
+//        }
+//    }
+//    
+//    //Delegate & DataSource
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return placeResults.count
+//    }
+//    
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath) as! DicoverCardCell
+//        cell.result = placeResults[indexPath.row]
+//        return cell
+//    }
 //
-
-import UIKit
-
-class DiscoverGroupHorizontalController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout {
-    
-    var category: String! {
-        didSet {
-            fetchPlaces(for: category)
-        }
-    }
-    
-    var placeResults = [PlaceResult]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.backgroundColor = .white
-        collectionView.register(DicoverCardCell.self, forCellWithReuseIdentifier: "id")
-        collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
-        
-        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
-            layout.scrollDirection = .horizontal
-        }
-        
-    }
-    
-    fileprivate func fetchPlaces(for category: String) {
-        Service.shared.fetchPlacesForCategory(for: category) { (results, error) in
-            
-            if let error = error {
-                print("Failed to Fetch: \(error.localizedDescription)")
-                return
-            }
-            
-            //success
-            var filteredResults = [PlaceResult]()
-            
-            results?.results.forEach({ (result) in
-                if result.containsPhotos() {
-                    filteredResults.append(result)
-                }
-            })
-            
-            self.placeResults = filteredResults
-            
-            
-            DispatchQueue.main.async {
-                if self.placeResults.count == 0 {
-                    //PlaceHolder or alternative?
-                    return
-                } else {
-                    self.collectionView.reloadData()
-                }
-            }
-        }
-    }
-    
-    //Delegate & DataSource
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return placeResults.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath) as! DicoverCardCell
-        cell.result = placeResults[indexPath.row]
-        return cell
-    }
-
-    //Layout
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: 180, height: view.frame.height - 16)
-    }
-}
-
-
+//    //Layout
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return .init(width: 180, height: view.frame.height - 16)
+//    }
+//}
+//
+//
