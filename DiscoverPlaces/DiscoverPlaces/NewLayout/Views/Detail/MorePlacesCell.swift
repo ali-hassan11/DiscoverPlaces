a3464b0a1f9a39e0431375197f6bb804d540df59
Ali Hassan <ali.software.dev@gmail.com>
2020-02-22 21:07:57 +0000

    Prelim more cells layout

--- DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/MorePlacesCell.swift
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/MorePlacesCell.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class MorePlacesCell: UICollectionViewCell {
     
+    let placeImageView = UIImageView(image: UIImage(named: "hotel"))
+     let placeNameLabel = UILabel(text: "Burj Khalifah Hotel - Dubai", font: .systemFont(ofSize: 17, weight: .semibold), color: .white, numberOfLines: 3)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        backgroundColor = .secondarySystemBackground
 
         layer.cornerRadius = 10
         clipsToBounds = true
-
-        backgroundColor = .secondarySystemBackground
         
+        placeImageView.contentMode = .scaleAspectFill
+        addSubview(placeImageView)
+        placeImageView.fillSuperview()
+        
+        addSubview(placeNameLabel)
+        placeNameLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 12, bottom: 12, right: 12))
         
     }
     
