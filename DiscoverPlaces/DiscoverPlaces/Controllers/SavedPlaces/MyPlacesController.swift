4796410038289bccbb70b11fae566538ce3b80dd
Ali Hassan <ali.software.dev@gmail.com>
2020-02-27 22:01:01 +0000

    MyList Cell stack view

--- DiscoverPlaces/DiscoverPlaces/Controllers/SavedPlaces/MyPlacesController.swift
+++ DiscoverPlaces/DiscoverPlaces/Controllers/SavedPlaces/MyPlacesController.swift
@@ -210,25 +210,46 @@ class MyListCell: UICollectionViewCell {
         
     }
 
-    let placeImageView = UIImageView(conrnerRadius: 10)
-    let placeNameLabel = UILabel(text: "Burj Khalifah", font: .systemFont(ofSize: 26, weight: .semibold), color: .label, numberOfLines: 1)
-    let addressLabel = UILabel(text: "123 Palace Road, London", font: .systemFont(ofSize: 16, weight: .medium), color: .label, alignment: .left, numberOfLines: 1)
-    let starView = StarsView()
-    let iconImageView = UIImageView(conrnerRadius: 0, width: 44, height: 44)
+    let placeImageView = UIImageView(image: UIImage(named: "pool"))
+    let placeNameLabel = UILabel(text: "Burj Khalifah", font: .systemFont(ofSize: 18, weight: .semibold), color: .label, numberOfLines: 1)
+    let addressLabel = UILabel(text: "123 Palace Road, London", font: .systemFont(ofSize: 15, weight: .medium), color: .label, alignment: .left, numberOfLines: 1)
+    let starView = UIView() //STARSVIEW
+    let iconImageView = UIImageView(image: UIImage(systemName: "heart"))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        layer.cornerRadius = 10
-        clipsToBounds = true
         
         backgroundColor = .systemBackground
         
-        addSubview(placeNameLabel)
-        placeNameLabel.fillSuperview()
+        placeImageView.layer.cornerRadius = 10
+        placeImageView.clipsToBounds = true
+        placeImageView.contentMode = .scaleAspectFill
+        placeImageView.constrainWidth(constant: 60)
+        placeImageView.constrainHeight(constant: 60)
+        
+        starView.backgroundColor = .systemPink
+        starView.constrainHeight(constant: 18)
+        starView.constrainWidth(constant: 100)
+        
+        iconImageView.tintColor = .systemPink
+        iconImageView.constrainWidth(constant: 30)
+        iconImageView.constrainHeight(constant: 30)
+        
+        
+        // ------------------CONFIGURE STACK VIEW ------------------ //
+        let labelsStackView = VerticalStackView(arrangedSubviews: [placeNameLabel, addressLabel, starView])
+        labelsStackView.setCustomSpacing(6, after: addressLabel)
+        labelsStackView.alignment = .leading
+        
+        let stackView = HorizontalStackView(arrangedSubviews: [placeImageView, labelsStackView, iconImageView], spacing: 12)
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 4, bottom: 12, right: 4))
         
         addBottomSeparator()
     }
-    
+        
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
