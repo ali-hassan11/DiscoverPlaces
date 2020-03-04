36646d3bf28a806765fd54d98e47ad06b85837c1
Ali Hassan <ali.software.dev@gmail.com>
2020-03-04 20:44:05 +0000

    Copied from favouriteController

--- DiscoverPlaces/DiscoverPlaces/NewLayout/Controllers/MyPlaces/ToDoListController.swift
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Controllers/MyPlaces/ToDoListController.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class ToDoListController: BaseCollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    var placeIdList: [String]?
+    var didSelectHandler: ((String) -> ())?
+
+    var placeIdList: [String]? {
+        didSet {
+            if placeIdList != oldValue {
+                fetchDataForPlaceIds()
+            }
+        }
+    }
+    
+    var placeResults: [PlaceDetailResult]?
     
     let defaults = DefaultsManager()
     
@@ -21,35 +31,61 @@ class ToDoListController: BaseCollectionViewController, UICollectionViewDelegate
         collectionView.register(MyPlaceCell.self, forCellWithReuseIdentifier: MyPlaceCell.id)
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        refreshData()
-//    }
-    
     func refreshData() {
         placeIdList = defaults.getList(listKey: .toDo)
         collectionView.reloadData()
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return placeIdList?.count ?? 0
+        return placeResults?.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyPlaceCell.id, for: indexPath) as! MyPlaceCell
         cell.listType = .toDo
-//        cell.placeId = placeIdList?[indexPath.item]
+        cell.place = placeResults?[indexPath.item]
         return cell
     }
-    
+        
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let detailsController = PlaceDetailsController()
-        detailsController.placeId = placeIdList?[indexPath.item]
-        present(detailsController, animated: true, completion: nil)//Change to push
+        guard let placeId = placeResults?[indexPath.item].place_id else { return }
+        didSelectHandler?(placeId)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.frame.width - 16 - 16, height: 100)
     }
     
+    func fetchDataForPlaceIds() {
+        self.placeResults?.removeAll()
+        placeIdList?.forEach({ (id) in
+            fetchData(for: id)
+        })
+    }
+    
+    func fetchData(for id: String) {
+        
+        let fields = ["name", "vicinity", "rating", "place_id", "photos"]
+        Service.shared.fetchPlaceDetails(placeId: id, fields: fields) { (response, error) in
+            
+            if let error = error {
+                print("Failed to fetch: \(error)")
+                return
+            }
+            
+            //success
+            guard let placeResponse = response else { return }
+            guard let result = placeResponse.result else { return }
+            
+            if self.placeResults != nil {
+                self.placeResults?.append(result)
+            } else {
+                self.placeResults = [result]
+            }
+            
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
 }
