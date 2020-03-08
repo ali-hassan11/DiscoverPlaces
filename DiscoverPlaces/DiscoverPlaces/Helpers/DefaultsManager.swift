ff036c10437ac625475e402250114a2c0f395b83
Ali Hassan <ali.software.dev@gmail.com>
2020-03-08 07:16:25 +0000

    Tidied defaults manager

--- DiscoverPlaces/DiscoverPlaces/Helpers/DefaultsManager.swift
+++ DiscoverPlaces/DiscoverPlaces/Helpers/DefaultsManager.swift
@@ -32,41 +32,43 @@ class ListObject: NSObject, NSCoding {
 }
 // // // ------------------- LOOK MORE INTO THIS ------------------- // // //
 
+
+// MARK: Lists
 class DefaultsManager {
     
-    func addToList(placeId: String, listKey: ListType) {
-        //CAN MAKE THIS ALOT SIMPLE: LOOK AT ADD TO CATEGORIES
-        if var favourites = UserDefaults.standard.object(forKey: listKey.rawValue) as? [String] {
+    private static let defaults = UserDefaults.standard
+    
+    static func addToList(placeId: String, listKey: ListType) {
+        if var favourites = defaults.object(forKey: listKey.rawValue) as? [String] {
             if !favourites.contains(placeId) {
                 favourites.append(placeId)
-                UserDefaults.standard.set(favourites, forKey: listKey.rawValue)
+                defaults.set(favourites, forKey: listKey.rawValue)
             }
         } else {
             let newFavourites = [placeId]
-            UserDefaults.standard.set(newFavourites, forKey: listKey.rawValue)
+            defaults.set(newFavourites, forKey: listKey.rawValue)
         }
     }
     
-    func removeFromList(placeId: String, listKey: ListType) {
-        //CAN MAKE THIS ALOT SIMPLE: LOOK AT ADD TO CATEGORIES
-        if var favourites = UserDefaults.standard.object(forKey: listKey.rawValue) as? [String] {
+    static func removeFromList(placeId: String, listKey: ListType) {
+        if var favourites = defaults.object(forKey: listKey.rawValue) as? [String] {
             if favourites.contains(placeId) {
                 favourites = favourites.filter{$0 != placeId}
-                UserDefaults.standard.set(favourites, forKey: listKey.rawValue)
+                defaults.set(favourites, forKey: listKey.rawValue)
             }
         }
     }
     
-    func getList(listKey: ListType) -> [String] {
-        if let favourites = UserDefaults.standard.object(forKey: listKey.rawValue) as? [String] {
+    static func getList(listKey: ListType) -> [String] {
+        if let favourites = defaults.object(forKey: listKey.rawValue) as? [String] {
             return favourites
         } else {
             return []
         }
     }
     
-    func isInList(placeId: String, listKey: ListType) -> Bool {
-        if let favourites = UserDefaults.standard.object(forKey: listKey.rawValue) as? [String] {
+    static func isInList(placeId: String, listKey: ListType) -> Bool {
+        if let favourites = defaults.object(forKey: listKey.rawValue) as? [String] {
             if favourites.contains(placeId) {
                 return true
             } else {
