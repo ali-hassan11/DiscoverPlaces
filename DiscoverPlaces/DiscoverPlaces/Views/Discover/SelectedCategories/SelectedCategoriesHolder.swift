eebec17177c52459b8ac3070365962c8c0e07d74
Ali Hassan <ali.software.dev@gmail.com>
2020-03-08 20:48:22 +0000

    Removed Old discover controllers folder

--- DiscoverPlaces/DiscoverPlaces/Views/Discover/SelectedCategories/SelectedCategoriesHolder.swift
+++ DiscoverPlaces/DiscoverPlaces/Views/Discover/SelectedCategories/SelectedCategoriesHolder.swift
@@ -1,29 +1,29 @@
+////
+////  SelectedCategoriesHolder.swift
+////  DiscoverPlaces
+////
+////  Created by user on 03/02/2020.
+////  Copyright © 2020 AHApps. All rights reserved.
+////
 //
-//  SelectedCategoriesHolder.swift
-//  DiscoverPlaces
+//import UIKit
 //
-//  Created by user on 03/02/2020.
-//  Copyright © 2020 AHApps. All rights reserved.
-//
-
-import UIKit
-
-class SelectedCategoriesHolder: UICollectionViewCell {
-        
-    let horizontalController = SelectedCategoresHorizontalController()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        backgroundColor = .red
-        
-        addSubview(horizontalController.view)
-        horizontalController.view.fillSuperview()
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-}
+//class SelectedCategoriesHolder: UICollectionViewCell {
+//        
+//    let horizontalController = SelectedCategoresHorizontalController()
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        
+//        backgroundColor = .red
+//        
+//        addSubview(horizontalController.view)
+//        horizontalController.view.fillSuperview()
+//        
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//}
