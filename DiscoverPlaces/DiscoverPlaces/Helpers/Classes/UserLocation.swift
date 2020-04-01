3bb9b994db673f00d6154436533088cf73b49df1
Ali Hassan <ali.software.dev@gmail.com>
2020-04-01 21:50:12 +0100

    Added UserLocation Class

--- DiscoverPlaces/DiscoverPlaces/Helpers/Classes/UserLocation.swift
+++ DiscoverPlaces/DiscoverPlaces/Helpers/Classes/UserLocation.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class UserLocation {
+    
+    static func lastSavedLocation() -> Location {
+        if let data = UserDefaults.standard.data(forKey: "Location") {
+            do {
+                let decoder = JSONDecoder()
+                let lastSavedLocation = try decoder.decode(Location.self, from: data)
+                return lastSavedLocation
+            } catch {
+                return Location(lat: 25.1412, lng: 55.1852) //Decide on a Default location (Currently Dubai)
+            }
+        } else {
+            return Location(lat: 25.1412, lng: 55.1852) //Decide on a Default location (Currently Dubai)
+        }
+    }
+    
+    //SaveLocation
+    
+}
