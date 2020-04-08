92d5aa13647b67ec534d9aa0cb14fb396fc80af0
Ali Hassan <ali.software.dev@gmail.com>
2020-04-08 23:06:17 +0100

    Removed StarView, added StarRatingView (Wrapper for CosmosView))

--- DiscoverPlaces/DiscoverPlaces/Helpers/Classes/StarRatingView.swift
+++ DiscoverPlaces/DiscoverPlaces/Helpers/Classes/StarRatingView.swift
@@ -1,13 +1,51 @@
 //
-//  StarRatingView.swift
+//  StarsView.swift
 //  DiscoverPlaces
 //
-//  Created by user on 02/04/2020.
+//  Created by user on 22/02/2020.
 //  Copyright © 2020 AHApps. All rights reserved.
 //
 
 import UIKit
+import Cosmos
 
-//public final class StarRatingView: UIControl {
-//    
-//}
+class StarRatingView: UIView {
+    
+    let cosmosView = CosmosView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSettings()
+        addToView()
+    }
+    
+    public func populate(with rating: Double, displaysNumber: Bool = false) {
+        cosmosView.rating = rating
+
+        if displaysNumber {
+            cosmosView.text = String(format: "%.1f", rating)
+        }
+    }
+    
+    private func configureSettings() {
+        cosmosView.settings.starMargin = 0
+        cosmosView.settings.starSize = 20
+        cosmosView.settings.fillMode = .precise
+        cosmosView.settings.emptyBorderWidth = 1
+        cosmosView.settings.emptyBorderColor = .systemPink
+        cosmosView.settings.filledBorderColor = .systemPink
+        cosmosView.settings.filledColor = .systemPink
+        cosmosView.settings.disablePanGestures = true
+        cosmosView.settings.updateOnTouch = false
+    }
+    
+    private func addToView() {
+        addSubview(cosmosView)
+        cosmosView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
