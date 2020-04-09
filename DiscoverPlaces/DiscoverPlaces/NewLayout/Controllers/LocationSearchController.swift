3f159a0184e2677fcd0441a799a7804e8d9eb024
Ali Hassan <ali.software.dev@gmail.com>
2020-04-09 17:55:18 +0100

    Added LocationSearchController
    Added file

---
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Controllers/LocationSearchController.swift
@@ -0,0 +1,69 @@
+//
+//  LocationSearchController.swift
+//  DiscoverPlaces
+//
+//  Created by user on 09/04/2020.
+//  Copyright © 2020 AHApps. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+
+class LocationSearchController: UITableViewController {
+    
+    private static let googlePlacesApiKey = "AIzaSyAgIjIKhiEllBtS2f_OSGTxZyHSJI-lXpg"
+    
+    var resultsCompletionHandler: ((Location?, String?) -> ())?
+    
+    private var resultsViewController: GMSAutocompleteResultsViewController?
+    private var searchController: UISearchController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Set Location"
+        
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        GMSPlacesClient.provideAPIKey(LocationSearchController.googlePlacesApiKey)
+        
+        resultsViewController = GMSAutocompleteResultsViewController()
+        resultsViewController?.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+        
+        searchController?.searchBar.sizeToFit()
+        navigationItem.searchController = searchController
+        
+        definesPresentationContext = true
+        
+        searchController?.hidesNavigationBarDuringPresentation = false
+        
+        resultsViewController?.tintColor = .systemPink
+        resultsViewController?.primaryTextColor = .label
+        resultsViewController?.secondaryTextColor = .secondaryLabel
+        resultsViewController?.tableCellSeparatorColor = .separator
+        resultsViewController?.tableCellBackgroundColor = .systemBackground
+    }
+    
+}
+
+extension LocationSearchController: GMSAutocompleteResultsViewControllerDelegate {
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didAutocompleteWith place: GMSPlace) {
+        searchController?.isActive = false
+        
+        let location = Location(lat: place.coordinate.latitude, lng: place.coordinate.longitude)
+        
+        self.resultsCompletionHandler?(location, place.name)
+    }
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
+//        resultsCompletionHandler?(nil, nil, error)
+        print("🚨🚨🚨 FAILED TO AUTOCOMPLETE 🚨🚨🚨")
+    }
+    
+}
