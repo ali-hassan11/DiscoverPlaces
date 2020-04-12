f0e11a75c4a701bcd27127658e783ccecfc70aed
Ali Hassan <ali.software.dev@gmail.com>
2020-04-12 11:01:07 +0100

    Connected didTapHanders

--- DiscoverPlaces/DiscoverPlaces/NewLayout/Controllers/MyPlaces/MyPlacesViewController.swift
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Controllers/MyPlaces/MyPlacesViewController.swift
@@ -31,8 +31,7 @@ class MyPlacesViewController: UIViewController {
         setupSegmentedControl()
         setupListControllers()
         setupContraints()
-//        setupDidTapPlaceHandler()
-//        setupDidScrollHandler()
+        setupDidTapPlaceHandler()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -91,20 +90,18 @@ class MyPlacesViewController: UIViewController {
         listControllersContainer.anchor(top: listSelector.bottomAnchor, leading: view.leadingAnchor, bottom: view.layoutMarginsGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
     }
     
-//    private func setupDidTapPlaceHandler() {
-//        horizontalController.didReceiveDataToPassOnHandler = { [weak self] placeId, location in  //Don't think location this is used
-////            guard let userLocation = self?.location else { return }
-//            guard let location = self?.location else { return }
-//            let detailController = PlaceDetailsController(placeId: placeId, location: location)
-//            self?.navigationController?.pushViewController(detailController, animated: true)
-//        }
-//    }
-//
-//    private func setupDidScrollHandler() {
-//        horizontalController.didScrollMyPlacesController = { [weak self] nearestPage in
-//            self?.listSelector.selectedSegmentIndex = nearestPage
-//        }
-//    }
+    var didTapComletionHandler: ((String, Location) -> ())?
+    
+    private func setupDidTapPlaceHandler() {
+        didTapComletionHandler = { [weak self] placeId, location in  //Don't think location this is used
+            guard let location = self?.location else { return }
+            let detailController = PlaceDetailsController(placeId: placeId, location: location)
+            self?.navigationController?.pushViewController(detailController, animated: true)
+        }
+    
+        favouritesController.didSelectPlaceInListHandler = didTapComletionHandler
+        toDoController.didSelectPlaceInListHandler = didTapComletionHandler
+    }
 }
 
 
