7633d03430aba97688441a1f46cd83662d46ca63
Ali Hassan <ali.software.dev@gmail.com>
2020-04-23 17:17:55 +0100

    Moved UserLocation to separate file

--- DiscoverPlaces/DiscoverPlaces/Helpers/Classes/UserLocation.swift
+++ DiscoverPlaces/DiscoverPlaces/Helpers/Classes/UserLocation.swift
@@ -6,3 +6,25 @@
 //  Copyright © 2020 AHApps. All rights reserved.
 //
 
+import Foundation
+
+class UserLoation {
+    
+    static func lastSavedLocation() -> LocationItem {
+        if let data = UserDefaults.standard.data(forKey: "LocationKey") {
+            do {
+                let decoder = JSONDecoder()
+                let lastSavedLocation = try decoder.decode(LocationItem.self, from: data)
+                return lastSavedLocation
+            } catch {
+                return LocationItem(name: "Dubai", selectedLocation:Location(lat: 25.1412, lng: 55.1852), actualUserLocation: nil) //Decide on a Default location (Currently Dubai)
+            }
+        } else {
+            return LocationItem(name: "Dubai", selectedLocation:Location(lat: 25.1412, lng: 55.1852), actualUserLocation: nil) //Decide on a Default location (Currently Dubai)
+        }
+    }
+    
+    //func SaveLocation() {
+    //}
+    
+}
