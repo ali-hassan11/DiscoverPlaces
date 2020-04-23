82a5ca94f7540a571f6e194594045390acc17f9b
Ali Hassan <ali.software.dev@gmail.com>
2020-04-23 14:58:28 +0100

    Clearded out old starViewTests

---
+++ DiscoverPlaces/DiscoverPlacesTests/BLANKTests.swift
@@ -0,0 +1,23 @@
+//
+//  DiscoverPlacesTests.swift
+//  DiscoverPlacesTests
+//
+//  Created by user on 26/01/2020.
+//  Copyright © 2020 AHApps. All rights reserved.
+//
+
+import XCTest
+@testable import DiscoverPlaces
+
+class BLANKTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+}
