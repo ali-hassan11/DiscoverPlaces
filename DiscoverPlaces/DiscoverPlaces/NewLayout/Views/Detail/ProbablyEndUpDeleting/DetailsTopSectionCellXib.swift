50c8019fbfc1f7939cf36288d3ae1a2a5c952bed
Ali Hassan <ali.software.dev@gmail.com>
2020-05-06 00:53:38 +0100

    Rearranged files

--- DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/ProbablyEndUpDeleting/DetailsTopSectionCellXib.swift
+++ DiscoverPlaces/DiscoverPlaces/NewLayout/Views/Detail/ProbablyEndUpDeleting/DetailsTopSectionCellXib.swift
@@ -1,49 +1,49 @@
+////
+////  TopSectionDetailsCell.swift
+////  DiscoverPlaces
+////
+////  Created by user on 09/02/2020.
+////  Copyright © 2020 AHApps. All rights reserved.
+////
 //
-//  TopSectionDetailsCell.swift
-//  DiscoverPlaces
+//import UIKit
 //
-//  Created by user on 09/02/2020.
-//  Copyright © 2020 AHApps. All rights reserved.
+//class DetailsTopSectionCellXib: UICollectionViewCell {
+//    
+//    var place: PlaceResult? {
+//        didSet {
+////            placeNameLabel.text = place?.name
+////            addressLabel.text = place?.vicinity
+//        }
+//    }
+//    
+//    //Sync label font with cell
+//    @IBOutlet weak var placeNameLabel: UILabel!
+//    @IBOutlet weak var addressLabel: UILabel!
+////    let placeNameLabel = UILabel(text: "Burj Al Arab", font: .boldSystemFont(ofSize: 24), color: .label, alignment: .center, numberOfLines: 0)
+//    
+////    let addressLabel = UILabel(text: "123 Buckingham Place Place, Victoria, London, United Kingdom, E17 7AJ", font: .systemFont(ofSize: 19), color: .secondaryLabel, alignment: .center, numberOfLines: 0)
 //
-
-import UIKit
-
-class DetailsTopSectionCellXib: UICollectionViewCell {
-    
-    var place: PlaceResult? {
-        didSet {
-//            placeNameLabel.text = place?.name
-//            addressLabel.text = place?.vicinity
-        }
-    }
-    
-    //Sync label font with cell
-    @IBOutlet weak var placeNameLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
-//    let placeNameLabel = UILabel(text: "Burj Al Arab", font: .boldSystemFont(ofSize: 24), color: .label, alignment: .center, numberOfLines: 0)
-    
-//    let addressLabel = UILabel(text: "123 Buckingham Place Place, Victoria, London, United Kingdom, E17 7AJ", font: .systemFont(ofSize: 19), color: .secondaryLabel, alignment: .center, numberOfLines: 0)
-
-//    let starView: UIView! = {
-//        let v = UIView()
-//        v.backgroundColor = UIColor.systemPink
-//        v.constrainHeight(constant: 22)
-//        v.constrainWidth(constant: 115)
-//        return v
-//    }()
-
-//    let reviewsButton: UIButton! = {
-//        let btn = UIButton()
-//        btn.setImage(UIImage(named: "reviewsButton"), for: .normal)
-//        btn.imageView?.contentMode = .scaleAspectFit
-//        btn.constrainWidth(constant: 80)
-//        return btn
-//    }()
-
-    
-    func configure(with place: PlaceDetailResult?) {
-        placeNameLabel.text = place?.name
-        addressLabel.text = place?.vicinity
-    }
-    
-}
+////    let starView: UIView! = {
+////        let v = UIView()
+////        v.backgroundColor = UIColor.systemPink
+////        v.constrainHeight(constant: 22)
+////        v.constrainWidth(constant: 115)
+////        return v
+////    }()
+//
+////    let reviewsButton: UIButton! = {
+////        let btn = UIButton()
+////        btn.setImage(UIImage(named: "reviewsButton"), for: .normal)
+////        btn.imageView?.contentMode = .scaleAspectFit
+////        btn.constrainWidth(constant: 80)
+////        return btn
+////    }()
+//
+//    
+//    func configure(with place: PlaceDetailResult?) {
+//        placeNameLabel.text = place?.name
+//        addressLabel.text = place?.vicinity
+//    }
+//    
+//}
